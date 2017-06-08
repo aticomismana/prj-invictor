9057368134b44aacc9c1897f192ddbc11805299f
Mateus Vinicius <mateusviniciussicat@hotmail.com>
2017-06-08 15:31:23 -0300

    implementa modelagem inicial

--- source/prj-invictor/Models/Usuario.cs
+++ source/prj-invictor/Models/Usuario.cs
@@ -7,5 +7,48 @@ namespace prj_invictor.Models
 {
     public class Usuario
     {
+        protected String nome { get; set; }
+        protected String sobrenome { get; set; }
+        protected String cpf { get; set; }
+        protected List<Impedimento> impedimentos;
+        protected float peso { get; set; }
+        protected DateTime nascimento { get; set; }
+        protected char sexo { get; set; }
+
+        public Boolean verificarPeso(char sexo)
+        {
+            return false;
+        }
+        public Boolean verificarIdade(char sexo)
+        {
+            return false;
+        }
+
+        public Boolean verificarCpf()
+        {
+            return false;
+        }
+        public Boolean verificarSexo(char sexo)
+        {
+            return false;
+
+        }
+        public void atualizarUsuario()
+        {
+
+        }
+        public void exibirCadastroUsr()
+        {
+
+        }
+        public void consultarCadastroUsr()
+        {
+
+        }
+        public void cadastroUsuario()
+        {
+
+        }
+
     }
 }
