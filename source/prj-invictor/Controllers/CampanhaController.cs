a3eebdfda4ac51ead174858b29f03811d815ae35
thiagoN13 <thiago.nogueira187@gmail.com>
2017-06-08 17:01:06 -0300

    Subindo alteracoes

---
+++ source/prj-invictor/Controllers/CampanhaController.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using prj_invictor.Models;
+
+namespace prj_invictor.Controllers
+{
+
+    [RoutePrefix("api/campanha")]
+    public class campanhaController : ApiController
+    {
+        private static List<CampanhaModel> listacampanhas = new List<CampanhaModel>();
+
+        [AcceptVerbs("POST")]
+        [Route("Cadastrarcampanha")]
+        public string CadastrarCampanha(CampanhaModel campanha)
+        {
+
+            listacampanhas.Add(campanha);
+
+            return "Usuário cadastrado com sucesso!";
+        }
+
+        [AcceptVerbs("PUT")]
+        [Route("Alterarcampanha")]
+        public string AlterarCampanha(CampanhaModel campanha)
+        {
+
+            listacampanhas.Where(n => n.Codigo == campanha.Codigo)
+                         .Select(s =>
+                         {
+                             s.Codigo = campanha.Codigo;
+                             s.Nome = campanha.Nome;
+                             s.Local = campanha.Local;
+                             s.DataInicio = campanha.DataInicio;
+                             s.DataFim = campanha.DataFim;
+                             s.DataPublicacao = campanha.DataPublicacao;
+
+                             return s;
+
+                         }).ToList();
+
+
+
+            return "Usuário alterado com sucesso!";
+        }
+
+        [AcceptVerbs("DELETE")]
+        [Route("Excluircampanha/{codigo}")]
+        public string ExcluirCampanha(int codigo)
+        {
+
+            CampanhaModel campanha = listacampanhas.Where(n => n.Codigo == codigo)
+                                                .Select(n => n)
+                                                .First();
+
+            listacampanhas.Remove(campanha);
+
+            return "Registro excluido com sucesso!";
+        }
+
+        [AcceptVerbs("GET")]
+        [Route("ConsultarcampanhaPorCodigo/{codigo}")]
+        public CampanhaModel ConsultarCampanhaPorCodigo(int codigo)
+        {
+
+            CampanhaModel campanha = listacampanhas.Where(n => n.Codigo == codigo)
+                                                .Select(n => n)
+                                                .FirstOrDefault();
+
+            return campanha;
+        }
+
+        [AcceptVerbs("GET")]
+        [Route("ConsultarCampanhas")]
+        public List<CampanhaModel> ConsultarCampanhas()
+        {
+            return listacampanhas;
+        }
+    }
+}
---
+++ source/prj-invictor/Models/Campanha.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace prj_invictor.Models
+{
+    public class CampanhaModel
+    {
+        public int Codigo { get; set; }
+        public string Nome { get; set; }
+        public string Local { get; set; }
+        public string DataInicio { get; set; }
+        public string DataFim { get; set; }
+        public string DataPublicacao { get; set; }
+
+
+        public CampanhaModel()
+        {
+        }
+
+        public CampanhaModel(int codigo, string local, string nome, string dataInicio, string dataFim, string dataPublicacao )
+        {
+            this.Codigo = codigo;
+            this.Nome = nome;
+            this.Local = local;
+            this.DataInicio = dataInicio;
+            this.DataFim = dataFim;
+            this.DataPublicacao = dataPublicacao;
+        }
+
+
+    }
+}
