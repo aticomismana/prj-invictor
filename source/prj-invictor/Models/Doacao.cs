b3c22cf880c8e54e6374c201eef9eac4f298fd08
Ático Mismana <aticomismana7@gmail.com>
2017-07-10 14:08:52 -0300

    implementação da classe Doacao

--- source/prj-invictor/Models/Doacao.cs
+++ source/prj-invictor/Models/Doacao.cs
@@ -1,55 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System;
 
 namespace prj_invictor.Models
 {
     class Doacao
     {
-       
-        List<Doacao> lista = new List<Doacao>();
-
-        public string Codigo { get; set; }
-        public DateTime DataDoacao { get; set; }
-	
-	public void GetAll()
-	{
-	     if(lista.Count <= 0) 
-	     {
-		return 0;
-	     }
-
-	 foreach (var campanha in lista)
-         {
-          	return (Usuario.(doacao.DataDoacao));
-         }
-}
-
-	public void Save(string codigo, DateTime dataDoacao)
-        {
-            Random rdn = new Random();
-
-            lista.Add( new Doacao() {
-                Codigo = rdn.Next(0, 2000).ToString(),
-                DataDoacao = dataDoacao,
-            };
-        }
+        public DateTime Data { get; set; }
+        public String Local { get; set; }
+        public String TipoDoacao { get; set; }
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
