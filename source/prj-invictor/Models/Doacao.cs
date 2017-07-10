cb11cc423590ce916b4962871c5a5f08af289a5d
FelipeWPS <felipewilliam321@gmail.com>
2017-07-10 15:01:42 -0300

    Update Doacao.cs

--- source/prj-invictor/Models/Doacao.cs
+++ source/prj-invictor/Models/Doacao.cs
@@ -1,11 +1,41 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace prj_invictor.Models
 {
     class Doacao
     {
-        public DateTime Data { get; set; }
-        public String Local { get; set; }
-        public String TipoDoacao { get; set; }
-    }
-}
+       
+        List<Doacao> lista = new List<Doacao>();
+
+        public string Codigo { get; set;}
+		public DateTime data {get; set;}
+        public string local { get; set; }
+        public string tipoDoacao { get; set; }
+  
+
+
+        public void GetAll()
+        {
+            if(lista.Count <= 0)
+            {
+                Console.WriteLine();
+                Console.WriteLine("==============================================================================");
+                Console.WriteLine("Não existe nenhuma doação cadastrada");
+                Console.WriteLine("==============================================================================");
+            }
+
+            foreach (var doacao in lista)
+            {
+                Console.WriteLine();
+                Console.WriteLine("==============================================================================");
+                Console.WriteLine("Codigo da doacao: {0}", doacao.Codigo);
+                Console.WriteLine("Nome da doação {0}", doacao.Doacao);
+                Console.WriteLine("Data da doacao: {0}", doacao.Data);
+                Console.WriteLine("Tipo Doacao: {0}" ,doacao.tipoDoacao);
+                Console.WriteLine("==============================================================================");
+            }
+        }
